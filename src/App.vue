e43ab0c9991b518aeaae6e9316c13e93a9032148
pemeraldy <pemeraldy@gmail.com>
2021-01-15 10:43:12 +0100

    mobile search bar and hero section

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <app-header />
+    <app-hero />
   </div>
 </template>
 
 <script>
 import AppHeader from "@/components/AppHeader";
+import AppHero from "@/components/AppHero";
 export default {
   name: "App",
   components: {
     AppHeader,
+    AppHero,
   },
 };
 </script>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="w-full relative px-8 bg-gray-700 flex flex-col justify-center items-center h-72 md:h-96"
+  >
+    <div
+      class="bg-no-repeat bg-cover bg-center bg-blue-100 rounded-md h-4/5 w-full relative"
+      style="background-image: url(https://picsum.photos/200/300);"
+    ></div>
+
+    <!-- Searh form -->
+    <div
+      class="w-full relative rounded-3xl -bottom-8 overflow-hidden shadow-lg "
+    >
+      <search-form />
+    </div>
+  </div>
+</template>
+
+<script>
+import SearchForm from "./SearchForm";
+export default {
+  components: {
+    SearchForm,
+  },
+};
+</script>
+
+<style></style>
