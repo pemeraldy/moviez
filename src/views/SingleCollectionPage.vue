c09ebb2f090b2f8b0af5bcad525b97073a3db2b4
pemeraldy <pemeraldy@gmail.com>
2021-01-18 14:26:58 +0100

    pading added to movie collections

--- src/views/SingleCollectionPage.vue
+++ src/views/SingleCollectionPage.vue
@@ -1,28 +1,28 @@
 <template>
-  <div class="h-screen w-screen">
+  <div class="h-screen w-screen mt-14 ">
     <div class="md:container h-full mx-auto">
-      <div class=" flex w-full h-2/3 bg-gray-900 ">
+      <div class=" flex w-full h-2/3 bg-gray-900 rounded-t-lg ">
         <div class="bg-red-30 w-4/6 flex-none"></div>
         <div class="bg-red-500 flex-auto space-y-4 p-4 overflow-auto ">
           <collection-card v-for="l in 14" :key="l">
-            <router-link class="block" tag="a" to="#">
-              <template v-slot:image>
-                <div class=" flex-none w-2/5 md:w-2/5">
-                  <img
-                    class="w-full h-full object-cover object-center"
-                    src="https://picsum.photos/900/720"
-                    alt="collection image"
-                  />
-                </div>
-              </template>
-              <template v-slot:content>
-                <div class="flex flex-col justify-between">
-                  <h1 class="font-bold">Movie Name</h1>
-                  <p class="mb-4 font-sans text-gray-500">2019</p>
-                  <p>3k like . <span>24 days ago</span></p>
-                </div>
-              </template>
-            </router-link>
+            <!-- <router-link class="block" tag="div" to="#"> -->
+            <template v-slot:image>
+              <div class=" flex-none w-2/5 md:w-2/5">
+                <img
+                  class="w-full h-full object-cover object-center"
+                  src="https://picsum.photos/900/720"
+                  alt="collection image"
+                />
+              </div>
+            </template>
+            <template v-slot:content>
+              <div class="flex flex-col justify-between">
+                <h1 class="font-bold">Movie Name asknclknalcnkas</h1>
+                <p class="mb-4 font-sans text-gray-400">2019</p>
+                <p>3k like . <span>24 days ago</span></p>
+              </div>
+            </template>
+            <!-- </router-link> -->
           </collection-card>
         </div>
       </div>
